65b4fb6309e561ba50259b1e95bdaba87e8c7abd
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2020-02-10 12:30:15 -0600

    update

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ cd build
 
 export CFLAGS="${CFLAGS} -I${CONDA_PREFIX}/include"
 export CXXFLAGS="${CXXFLAGS} -I${CONDA_PREFIX}/include"
-#export CXXFLAGS="${CXXFLAGS} -I${CONDA_PREFIX}/include -pedantic -Wno-long-long -fno-inline"
 
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
 
 make VERBOSE=1
-#make install
+make install
